9bc8d1a2e29cef491cc6da7695f73e35178e126a
Brian Schubert <schubert.b@northeastern.edu>
2023-11-04 20:03:53 -0400

    Add updated emulator skeleton

--- src/gp_emulator/_base.py
+++ src/gp_emulator/_base.py
@@ -1,36 +1,93 @@
+from __future__ import annotations
+
 import abc
-from collections.abc import Mapping, Sequence
-from typing import Any, Literal
+import io
+from collections.abc import Iterator, Mapping, Sequence
+from typing import Any, Callable, Generic, Iterable, Literal, NamedTuple, TypeVar
 
 import numpy as np
+import xarray
 from nptyping import Floating, NDArray
 import xarray as xr
-from typing_extensions import TypeAlias
+from typing_extensions import Self, TypeAlias
 
 ExampleFeatureArray: TypeAlias = NDArray[Literal["* example, * feature"], Floating]
 """
 2D array with examples along the first dimension and features along the second dimension. 
 """
 
-CoordinateName: TypeAlias = str
+_DimName: TypeAlias = str
+
+_T = TypeVar("_T")
+
+_G = TypeVar("_G", bound="BaseGPEmulator")
+
+
+class MeanStd(NamedTuple, Generic[_T]):
+    """Tuple containing a mean and standard deviation."""
+
+    mean: _T
+    std: _T
 
 
 class BaseGPEmulator(abc.ABC):
     """
-    Base class for Gaussian Process (GP) based emulators.
+    Base class for Gaussian Process (GP) emulators.
     """
 
-    dim_names: tuple[str, ...]
+    dim_names: tuple[_DimName, ...]
     """
     Fixed names of input parameters.
     
     All grid outputs will have these dimensions in this order.
     """
 
-    def __init__(self) -> None:
+    train_features: ExampleFeatureArray
+
+    train_targets: ExampleFeatureArray
+
+    def __init__(
+        self,
+        train_features: ExampleFeatureArray,
+        train_targets: train_targets,
+        dim_names: Iterable[_DimName],
+    ) -> None:
+        self.train_features = train_features
+        self.train_targets = train_targets
+        self.dim_names = tuple(dim_names)
+
+        x_shape = np.shape(train_features)
+        y_shape = np.shape(train_targets)
+
+        if not len(x_shape) == len(y_shape) == 2:
+            raise ValueError(
+                f"training features and targets must be 2D arrays, got shapes {x_shape} and {y_shape}"
+            )
+
+        if x_shape[0] != y_shape[0]:
+            raise ValueError(
+                "training features must contain the same number of rows as the the targets"
+            )
+
+        if y_shape[1] != 1:
+            raise ValueError("targets must have a single column")
+
+    @classmethod
+    def extract_dataarray(cls):
+        pass
+
+    @classmethod
+    @abc.abstractmethod
+    def load(cls, file: io.RawIOBase) -> Self:
         ...
 
-    def predict_grid(self, x: Mapping[CoordinateName, Sequence[Any]]):
+    @abc.abstractmethod
+    def train(self) -> None:
+        """Fit this emulator to its training dataset."""
+
+    def predict_grid(
+        self, x: Mapping[_DimName, Sequence[Any]]
+    ) -> MeanStd[xarray.DataArray]:
         expected_dims = set(self.dim_names)
         received_dims = set(x.keys())
 
@@ -63,10 +120,51 @@ class BaseGPEmulator(abc.ABC):
             std_examples.reshape(mesh_shape), coords=x, dims=self.dim_names
         )
 
-        return mean_grid, std_grid
+        return MeanStd(mean_grid, std_grid)
 
     @abc.abstractmethod
-    def predict(
-        self, x: ExampleFeatureArray
-    ) -> tuple[ExampleFeatureArray, ExampleFeatureArray]:
+    def predict(self, x: ExampleFeatureArray) -> MeanStd[ExampleFeatureArray]:
         ...
+
+    @abc.abstractmethod
+    def export(self, file: io.RawIOBase) -> None:
+        ...
+
+
+class MultiGPEmulator(Generic[_G]):
+    """
+    Collection of GPs that can be evaluated simultaneously.
+    """
+
+    component_dim: _DimName
+
+    component_names: list[Any]
+
+    components: list[_G]
+
+    component_factory: Callable[[], _G]
+
+    def __init__(
+        self,
+    ) -> None:
+        pass
+
+    @abc.abstractmethod
+    def predict_components_grid(
+        self,
+        x: Mapping[_DimName, Sequence[Any]],
+        which: Iterable[int] | None = None,
+        format: Literal["array", "dataset", "tuple"] = "array",
+    ) -> MeanStd[xr.DataArray]:
+        ...
+
+    @abc.abstractmethod
+    def predict_components(
+        self, x: ExampleFeatureArray, which: Iterable[int] | None = None
+    ) -> Iterator[MeanStd]:
+        if which is not None:
+            desired_components = [self.components[idx] for idx in which]
+        else:
+            desired_components = self.components
+
+        return (comp.predict(x) for comp in desired_components)
---
+++ src/gp_emulator/_skl.py
@@ -0,0 +1,43 @@
+"""
+Scikit-learn based GP emulators.
+"""
+import io
+import pickle
+import typing
+from typing import Any
+from typing_extensions import Self
+
+import sklearn.gaussian_process as skl_gp
+import sklearn.base as skl_base
+
+from ._base import BaseGPEmulator, ExampleFeatureArray
+
+
+class SklearnGPEmulator(BaseGPEmulator):
+    """Emulator using Scikit-learn's GP implementation."""
+
+    gaussian_process: skl_gp.GaussianProcessRegressor
+
+    def __init__(self, gaussian_process, *args: Any, **kwargs: Any) -> None:
+        self.gaussian_process = typing.cast(
+            skl_gp.GaussianProcessRegressor,
+            skl_base.clone(gaussian_process),
+        )
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def load(cls, file: io.RawIOBase) -> Self:
+        return typing.cast(Self, pickle.load(file))
+
+    def predict(
+        self, x: ExampleFeatureArray
+    ) -> tuple[ExampleFeatureArray, ExampleFeatureArray]:
+        return self.gaussian_process.predict(x, return_std=True)
+
+    def export(self, file: io.RawIOBase) -> None:
+        # Scikit-learn explicitly supports serializing models via pickling
+        # https://scikit-learn.org/stable/model_persistence.html
+        return pickle.dump(self, file)
+
+    def train(self) -> None:
+        self.gaussian_process.fit(self.train_features, self.train_targets)
